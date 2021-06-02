a4cf82b11bad251eaeebe48120cfc2a772b460e6
Jiří Vrba <jiri@vrba.dev>
2021-06-02 21:49:48 +0200

    Initial commit

---
+++ src/main/java/dev/vrba/onlypets/OnlyPetsApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.onlypets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyPetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vrba/onlypets/OnlyPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vrba.onlypets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyPetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
