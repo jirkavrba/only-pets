15f4aece9d1dc3beb68000fc77b4ee75aaf14895
Jiří Vrba <jiri@vrba.dev>
2021-06-03 23:08:17 +0200

    PetDTO + WithOwner variant

---
+++ src/main/java/dev/vrba/onlypets/entity/dto/PetDTO.java
@@ -0,0 +1,46 @@
+package dev.vrba.onlypets.entity.dto;
+
+import dev.vrba.onlypets.entity.Pet;
+import dev.vrba.onlypets.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class PetDTO {
+    @NotNull
+    private final UUID id;
+
+    @NotNull
+    private final String name;
+
+    @Nullable
+    private final Date birthdate;
+
+    @Nullable
+    private final String paypalLink;
+
+    public PetDTO(@NotNull Pet pet) {
+        this.id = pet.getId();
+        this.name = pet.getName();
+        this.birthdate = pet.getBirthdate();
+        this.paypalLink = pet.getPaypalLink();
+    }
+
+    @Getter
+    public static class WithOwner extends PetDTO {
+        @NotNull
+        private final User owner;
+
+        public WithOwner(@NotNull Pet pet) {
+            super(pet);
+            this.owner = pet.getOwner();
+        }
+    }
+}
