06b8ac901bce5de166acd92a6f5fffff094975bf
Jiří Vrba <jiri@vrba.dev>
2021-06-04 23:46:46 +0200

    Fix AWS configuration

--- src/main/java/dev/vrba/onlypets/configuration/AWSConfiguration.java
+++ src/main/java/dev/vrba/onlypets/configuration/AWSConfiguration.java
@@ -6,14 +6,11 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import lombok.AllArgsConstructor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AllArgsConstructor
-@ConfigurationProperties(prefix = "aws")
 public class AWSConfiguration {
 
     private final String accessKey;
@@ -21,6 +18,16 @@ public class AWSConfiguration {
     private final String secretKey;
 
     private final String region;
+
+    public AWSConfiguration(
+            @Value("${aws.access-key}") String accessKey,
+            @Value("${aws.secret-key}") String secretKey,
+            @Value("${aws.region}") String region
+    ) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.region = region;
+    }
 
     @Bean
     public AmazonS3 s3() {
