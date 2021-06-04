a5bed158f080eb741e2144f445e627c10abe44cc
Jiří Vrba <jiri@vrba.dev>
2021-06-04 22:57:39 +0200

    Add AWS configuration

---
+++ src/main/java/dev/vrba/onlypets/configuration/AWSConfiguration.java
@@ -0,0 +1,35 @@
+package dev.vrba.onlypets.configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "aws")
+public class AWSConfiguration {
+
+    private final String accessKey;
+
+    private final String secretKey;
+
+    private final String region;
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        AWSCredentialsProvider provider = new AWSStaticCredentialsProvider(credentials);
+
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(this.region)
+                .withCredentials(provider)
+                .build();
+    }
+}
